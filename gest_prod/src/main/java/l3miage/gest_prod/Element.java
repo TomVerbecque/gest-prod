d987ad63af64ff217bf048b2b4855edb1c13cf0b
Tom Verbecque <101146235+TomVerbecque@users.noreply.github.com>
2024-04-01 22:06:58 +0200

    mise en place des classes de base

---
+++ gest_prod/src/main/java/l3miage/gest_prod/Element.java
@@ -0,0 +1,51 @@
+package l3miage.gest_prod;
+
+public class Element {
+	
+    private String code;
+    private String nom;
+    private double qteStock;
+    private String unite;
+
+    public Element(String p_code, String p_nom, int p_qteStock, String p_unite) {
+        this.code = p_code;
+        this.nom = p_nom;
+        this.qteStock = p_qteStock;
+        this.unite = p_unite;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public double getQteStock() {
+        return this.qteStock;
+    }
+    public void setQteStock(double quantite) {
+        this.qteStock = quantite;
+    }
+
+    public String getUniteMesure() {
+        return this.unite;
+    }
+
+    public void ajouterQteStock(double qte) {
+        this.qteStock += qte;
+    }
+    
+    public void soustraireQteStock(double qte) {
+        this.qteStock -= qte;
+    }
+
+    @Override
+    public String toString() {
+        return "Element " + this.code + '\'' +
+                ", nom : " + this.nom + '\'' +
+                ", quantité en stock : " + this.qteStock +
+                ", unité de mesure : '" + this.unite;
+    }
+}
