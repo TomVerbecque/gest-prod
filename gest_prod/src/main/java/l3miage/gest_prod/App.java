4b324a29929893fec83cd14e817014c70d5111fc
ThomasSchmdt <schmidt.thomas.pro@gmail.com>
2024-04-03 20:28:32 +0200

    JavaFX Fichier + controller

--- gest_prod/src/main/java/l3miage/gest_prod/App.java
+++ gest_prod/src/main/java/l3miage/gest_prod/App.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 /*
@@ -47,19 +49,14 @@ public class App {
 */
 
 public class App extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	    Parent root = FXMLLoader.load(getClass().getResource("Accueil.fxml"));
+	    primaryStage.setScene(new Scene(root));
+	    primaryStage.show();
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
